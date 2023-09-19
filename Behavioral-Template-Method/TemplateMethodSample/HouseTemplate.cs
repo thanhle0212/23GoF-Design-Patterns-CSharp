3ab831d6432611d95d35a89f8b8fe3f31afc9f00
Thanh Le <letienthanh0212@gmail.com>
2023-09-18 23:18:58 -0400

    Template method design pattern
    Template method design pattern

---
+++ Behavioral-Template-Method/TemplateMethodSample/HouseTemplate.cs
@@ -0,0 +1,21 @@
+namespace TemplateMethodSample
+{
+    public abstract class HouseTemplate
+    {
+         // Template Method defines the sequence for building a house
+        public void BuildHouse()
+        {
+            //Define the Steps to Build a House
+            BuildFoundation(); //Step1
+            BuildPillars(); //Step2
+            BuildWalls(); //Step3
+            BuildWindows(); //Step4
+            Console.WriteLine("House is Built");
+        }
+        // Methods to be implemented by subclasses
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindows();
+    }
+}
---
+++ Behavioral-Template-Method/TemplateMethodSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using TemplateMethodSample;
+
+Console.WriteLine("Build a Concrete House\n");
+HouseTemplate houseTemplate = new ConcreteHouse();
+//Call the Template Method to Build the Concrete House
+houseTemplate.BuildHouse();
+Console.WriteLine();
+Console.WriteLine("Build a Wooden House\n");
+houseTemplate = new WoodenHouse();
+//Call the Template Method to Build the Wooden House
+houseTemplate.BuildHouse();
+Console.Read();
